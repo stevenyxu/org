a3e0fcf347409892577402395979fac80abc5881
Steven Xu <steven@stevenxu.ca>
2020-12-12 14:38:59 -0800

    Tailwind setup.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
 import Repos from './Repos.js';
 
 function App() {
   return (
     <>
-      <header className="App-header">
-        <h1>Org viewer</h1>
-        <span className="App-header-links">
-          <a href="https://github.com/stevenyxu/org-viewer">code</a>
-        </span>
+      <header className="bg-gray-800 flex items-center justify-between px-2 py-1">
+        <h1 className="text-white">Org viewer</h1>
+        <a className="text-gray-300" href="https://github.com/stevenyxu/org-viewer">code</a>
       </header>
-      <Repos></Repos>
+      <main className="p-2">
+        <Repos></Repos>
+      </main>
     </>
   );
 }
--- src/Repos.js
+++ src/Repos.js
@@ -11,23 +11,27 @@ export default function Repos(props) {
     }, [props.orgId]);
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {repos.map((repo) => {
-                    return (
-                        <tr key={repo['id']}>
-                            <td>{repo['id']}</td>
-                            <td>{repo['name']}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <div className="rounded-lg overflow-hidden shadow">
+            <table className="bg-white">
+                <thead>
+                    <tr className="text-left bg-gray-200 uppercase text-gray-500 text-xs">
+                        <th className="font-medium py-1 px-4">ID</th>
+                        <th className="font-medium py-1 px-4">Name</th>
+                        <th aria-hidden="true" className="w-full"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {repos.map((repo) => {
+                        return (
+                            <tr key={repo['id']}>
+                                <td className="py-1 px-4">{repo['id']}</td>
+                                <td className="py-1 px-4 w-1/2">{repo['name']}</td>
+                                <td aria-hidden="true" className="w-full"></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
